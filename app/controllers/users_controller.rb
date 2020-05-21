12c881deb1f249de4528724a5df8e1ba6b1f6090
marsha <marshaannon@gmail.com>
2020-05-21 09:57:47 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to "/welcome"
         else
-            redirect_to "new"
+            redirect_to "/users/new"
         end
     end
 
